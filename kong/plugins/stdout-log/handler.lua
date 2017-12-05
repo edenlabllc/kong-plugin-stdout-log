7ad5ca273b635294221c7e2c1fd183a52aab9bf8
Alex Kovalevych <alexkovalevych@gmail.com>
2017-12-05 14:37:22 +0200

    initial commit

---
+++ kong/plugins/stdout-log/handler.lua
@@ -0,0 +1,20 @@
+local cjson = require "cjson"
+local basic_serializer = require "kong.plugins.log-serializers.basic"
+
+local BasePlugin = require "kong.plugins.base_plugin"
+local StdoutLogHandler = BasePlugin:extend()
+
+StdoutLogHandler.PRIORITY = 9
+StdoutLogHandler.VERSION = "0.0.1"
+
+function StdoutLogHandler:new()
+  StdoutLogHandler.super.new(self, "stdout-log")
+end
+
+function StdoutLogHandler:log(conf)
+    StdoutLogHandler.super.log(self)
+    local message = basic_serializer.serialize(ngx)
+    ngx.log(ngx.NOTICE, cjson.encode(message))
+end
+
+return StdoutLogHandler
